7e8634739d76a4c072f70c1de283f6e8630e2de0
Fabien <7irelide@gmail.com>
2024-12-27 17:28:07 +0100

    first commit

---
+++ Start Interactif.ps1
@@ -0,0 +1,178 @@
+﻿# Variables globales
+$backupPath = "$env:USERPROFILE\Desktop\WifiBackup"
+$ssid = "Nom_de_Mon_WIFI"
+$key = "Password WIFi"
+$encryptionType = "WPA2PSK"
+
+function Show-Menu {
+    Clear-Host
+    Write-Host "╔══════════════════════════════════════════════════════╗" -ForegroundColor Blue
+    Write-Host "║                                                      ║" -ForegroundColor Blue
+    Write-Host "║            ██     ██    ██     ███████ ██            ║" -ForegroundColor Cyan
+    Write-Host "║            ██     ██    ██     ██      ██            ║" -ForegroundColor Cyan
+    Write-Host "║            ██  █  ██    ██     █████   ██            ║" -ForegroundColor Cyan
+    Write-Host "║            ██ ███ ██    ██     ██      ██            ║" -ForegroundColor Cyan
+    Write-Host "║             ███ ███     ██     ██      ██            ║" -ForegroundColor Cyan
+    Write-Host "║                                                      ║" -ForegroundColor Blue
+    Write-Host "║              Gestion des profils Wi-Fi               ║" -ForegroundColor Yellow
+    Write-Host "║                         FAFA                         ║" -ForegroundColor Blue
+    Write-Host "╠══════════════════════════════════════════════════════╣" -ForegroundColor Blue
+    Write-Host "║                                                      ║" -ForegroundColor Blue
+    Write-Host "║  [1] ⇒ Sauvegarder les profils Wi-Fi                 ║" -ForegroundColor Green
+    Write-Host "║  [2] ⇒ Connecter au Wi-Fi de l'entreprise            ║" -ForegroundColor Green
+    Write-Host "║  [3] ⇒ Supprimer tous les profils Wi-Fi              ║" -ForegroundColor Green
+    Write-Host "║  [4] ⇒ Restaurer les anciens profils                 ║" -ForegroundColor Green
+    Write-Host "║  [Q] ⇒ Quitter                                       ║" -ForegroundColor Green
+    Write-Host "║                                                      ║" -ForegroundColor Blue
+    Write-Host "╚══════════════════════════════════════════════════════╝" -ForegroundColor Blue
+}
+
+function Show-ProgressBar {
+    param (
+        [int]$PercentComplete
+    )
+    $width = 50
+    $complete = [math]::Floor($width * ($PercentComplete / 100))
+    $remaining = $width - $complete
+    $progressBar = "[" + "■" * $complete + "□" * $remaining + "]"
+    Write-Host "`r$progressBar $PercentComplete%" -NoNewline
+}
+
+function Backup-WifiProfiles {
+    if (!(Test-Path $backupPath)) {
+        New-Item -ItemType Directory -Path $backupPath | Out-Null
+    }
+    
+    $profiles = netsh wlan show profiles | Select-String "\:(.+)$"
+    $totalProfiles = $profiles.Count
+    $currentProfile = 0
+
+    foreach ($profile in $profiles) {
+        $name = $profile.Matches.Groups[1].Value.Trim()
+        $xmlPath = "$backupPath\$name.xml"
+        netsh wlan export profile name="$name" folder="$backupPath" key=clear | Out-Null
+        $currentProfile++
+        $percentComplete = [math]::Floor(($currentProfile / $totalProfiles) * 100)
+        Show-ProgressBar -PercentComplete $percentComplete
+    }
+    Write-Host "`n`nSauvegarde terminée !" -ForegroundColor Green
+}
+
+function Connect-EnterpriseWifi {
+    $profileXML = @"
+<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+    <name>$ssid</name>
+    <SSIDConfig>
+        <SSID>
+            <hex>376972656C6964655F536572766572203547487A</hex>
+            <name>$ssid</name>
+        </SSID>
+    </SSIDConfig>
+    <connectionType>ESS</connectionType>
+    <connectionMode>auto</connectionMode>
+    <MSM>
+        <security>
+            <authEncryption>
+                <authentication>$encryptionType</authentication>
+                <encryption>AES</encryption>
+                <useOneX>false</useOneX>
+            </authEncryption>
+            <sharedKey>
+                <keyType>passPhrase</keyType>
+                <protected>false</protected>
+                <keyMaterial>$key</keyMaterial>
+            </sharedKey>
+        </security>
+    </MSM>
+    <MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3">
+        <enableRandomization>false</enableRandomization>
+        <randomizationSeed>1717643993</randomizationSeed>
+    </MacRandomization>
+</WLANProfile>
+"@
+    $profilePath = "$env:TEMP\EnterpriseWiFiProfile.xml"
+    $profileXML | Out-File -FilePath $profilePath
+    
+    Write-Host "Ajout du profil Wi-Fi..." -NoNewline
+    netsh wlan add profile filename="$profilePath" | Out-Null
+    Write-Host " OK" -ForegroundColor Green
+
+    Write-Host "Connexion au réseau..." -NoNewline
+    netsh wlan connect name=$ssid | Out-Null
+    Write-Host " OK" -ForegroundColor Green
+
+    Remove-Item -Path $profilePath
+    Write-Host "`nConnexion au réseau Wi-Fi '$ssid' effectuée avec succès !" -ForegroundColor Green
+}
+
+function Remove-AllWifiProfiles {
+    $profiles = netsh wlan show profiles | Select-String "\:(.+)$"
+    $totalProfiles = $profiles.Count
+    $currentProfile = 0
+
+    foreach ($profile in $profiles) {
+        $name = $profile.Matches.Groups[1].Value.Trim()
+        netsh wlan delete profile name="$name" | Out-Null
+        $currentProfile++
+        $percentComplete = [math]::Floor(($currentProfile / $totalProfiles) * 100)
+        Show-ProgressBar -PercentComplete $percentComplete
+    }
+    Write-Host "`n`nTous les profils Wi-Fi ont été supprimés !" -ForegroundColor Yellow
+}
+
+function Restore-WifiProfiles {
+    if (!(Test-Path $backupPath)) {
+        Write-Host "Aucune sauvegarde trouvée." -ForegroundColor Red
+        return
+    }
+    $profiles = Get-ChildItem -Path $backupPath -Filter *.xml
+    $totalProfiles = $profiles.Count
+    $currentProfile = 0
+
+    foreach ($profile in $profiles) {
+        netsh wlan add profile filename="$($profile.FullName)" | Out-Null
+        $currentProfile++
+        $percentComplete = [math]::Floor(($currentProfile / $totalProfiles) * 100)
+        Show-ProgressBar -PercentComplete $percentComplete
+    }
+    Write-Host "`n`nRestauration des profils Wi-Fi terminée !" -ForegroundColor Green
+}
+
+# Boucle principale du menu
+do {
+    Show-Menu
+    $input = Read-Host "Entrez votre choix"
+    switch ($input) {
+        '1' {
+            Backup-WifiProfiles
+            pause
+        }
+        '2' {
+            Connect-EnterpriseWifi
+            pause
+        }
+        '3' {
+            Remove-AllWifiProfiles
+            pause
+        }
+        '4' {
+            Restore-WifiProfiles
+            pause
+        }
+        'q' {
+            Clear-Host
+            Write-Host "╔══════════════════════════════════════════════════════╗" -ForegroundColor Blue
+            Write-Host "║                                                      ║" -ForegroundColor Blue
+            Write-Host "║   Merci d'avoir utilisé le gestionnaire de Wi-Fi !   ║" -ForegroundColor Yellow
+            Write-Host "║                                                      ║" -ForegroundColor Blue
+            Write-Host "║                   À bientôt !                        ║" -ForegroundColor Yellow
+            Write-Host "║                                                      ║" -ForegroundColor Blue
+            Write-Host "╚══════════════════════════════════════════════════════╝" -ForegroundColor Blue
+            return
+        }
+        default {
+            Write-Host "Option non valide. Veuillez réessayer." -ForegroundColor Red
+            pause
+        }
+    }
+} while ($true)
---
+++ Start.ps1
@@ -0,0 +1,79 @@
+﻿# Variables
+$SSID = "7irelide_Server 5GHz"
+$Key = "WIFI-C10TR3M02P@S-WIFI"
+
+# - "WPA" : Wi-Fi Protected Access, première version. Plus sûr que WEP, mais moins sécurisé que WPA2 et WPA3. Utilisé rarement dans les configurations modernes.
+# - "WPA2PSK" : WPA2 (Wi-Fi Protected Access II) en mode clé pré-partagée (PSK). Actuellement le standard pour les réseaux domestiques et d'entreprises, combinant une bonne sécurité et une facilité de configuration.
+# - "WPA3SAE" : WPA3 (Wi-Fi Protected Access III) avec Simultaneous Authentication of Equals (SAE). Le mode le plus récent pour les réseaux personnels, offrant une sécurité accrue par rapport à WPA2.
+# - "WEP" : Wired Equivalent Privacy. Très ancien et non sécurisé, considéré comme obsolète. Recommandé d'éviter son utilisation.
+# - "open" : Pas de cryptage (réseau ouvert). Utilisé principalement pour les réseaux publics où aucune authentification n'est requise.
+# - "WPA2Enterprise" : WPA2 en mode entreprise, utilise 802.1X pour l'authentification (souvent avec un serveur RADIUS). Offrant une sécurité renforcée pour les réseaux professionnels.
+# - "WPA3Enterprise" : WPA3 en mode entreprise, avec des améliorations de sécurité par rapport à WPA2Enterprise. Utilisé pour les environnements nécessitant une sécurité renforcée.
+# - "WPA3Enterprise192" : WPA3 Enterprise avec cryptage de 192 bits, destiné aux environnements les plus sécurisés, conforme aux exigences de sécurité gouvernementales et militaires.
+
+$EncryptionType = "WPA2PSK"  # Options possibles: WPA, WPA2PSK, WPA3SAE, WEP, open, WPA2Enterprise, WPA3Enterprise, WPA3Enterprise192
+
+########################################################################################################
+# Vérification si le profil Wi-Fi existe déjà
+$existingProfile = netsh wlan show profiles | Select-String -Pattern $SSID
+
+if ($existingProfile) {
+    Write-Output "Le profil Wi-Fi '$SSID' existe déjà. Tentative de mise à jour des informations de sécurité..."
+    # Suppression du profil existant
+    $removeResult = netsh wlan delete profile name=$SSID
+    if ($removeResult) {
+        Write-Output "Le profil Wi-Fi existant a été supprimé."
+    } else {
+        Write-Output "Impossible de mettre à jour le profil existant. Il sera supprimé pour être recréé."
+    }
+} else {
+    Write-Output "Aucun profil existant trouvé pour '$SSID'. Un nouveau profil sera créé."
+}
+
+# Contenu du fichier XML pour le profil Wi-Fi
+$profileXML = @"
+<WLANProfile xmlns="http://www.microsoft.com/networking/WLAN/profile/v1">
+    <name>$SSID</name>
+    <SSIDConfig>
+        <SSID>
+            <hex>376972656C6964655F536572766572203547487A</hex> <!-- Hexadécimal pour '7irelide_Server 5GHz' -->
+            <name>$SSID</name>
+        </SSID>
+    </SSIDConfig>
+    <connectionType>ESS</connectionType>
+    <connectionMode>auto</connectionMode>
+    <MSM>
+        <security>
+            <authEncryption>
+                <authentication>$EncryptionType</authentication>
+                <encryption>AES</encryption>
+                <useOneX>false</useOneX>
+            </authEncryption>
+            <sharedKey>
+                <keyType>passPhrase</keyType>
+                <protected>false</protected>
+                <keyMaterial>$Key</keyMaterial>
+            </sharedKey>
+        </security>
+    </MSM>
+    <MacRandomization xmlns="http://www.microsoft.com/networking/WLAN/profile/v3">
+        <enableRandomization>false</enableRandomization>
+        <randomizationSeed>1717643993</randomizationSeed>
+    </MacRandomization>
+</WLANProfile>
+"@
+
+# Sauvegarde du profil dans un fichier temporaire
+$profilePath = "$env:TEMP\WiFiProfile.xml"
+$profileXML | Out-File -FilePath $profilePath
+
+# Ajout du profil Wi-Fi
+netsh wlan add profile filename="$profilePath"
+
+# Connexion au réseau Wi-Fi
+netsh wlan connect name=$SSID
+
+# Nettoyage du fichier XML temporaire
+Remove-Item -Path $profilePath
+
+Write-Output "Connexion au réseau Wi-Fi '$SSID' complétée."
